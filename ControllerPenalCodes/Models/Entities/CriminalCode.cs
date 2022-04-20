8ec8cbd902d9ffc428790a9b600c726cbab2b3d4
arprog <argit123@hotmail.com>
2022-04-20 16:20:06 -0300

    Creation of models

---
+++ ControllerPenalCodes/Models/Entities/CriminalCode.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ControllerPenalCodes.Models.Entities
+{
+	public class CriminalCode
+	{
+		public Guid Id { get; set; }
+
+		public string Name { get; set; }
+
+		public string Description { get; set; }
+
+		public decimal Penalty { get; set; }
+
+		public int PrisionTime { get; set; }
+
+		public Guid StatusId { get; set; }
+
+		public Status Status { get; set; }
+
+		public DateTime CreateDate { get; set; }
+
+		public DateTime UpdateDate { get; set; }
+
+		public Guid CreateUserId { get; set; }
+
+		public User CreateUser { get; set; }
+
+		public Guid UpdateUserId { get; set; }
+
+		public User UpdateUser { get; set; }
+	}
+}
---
+++ ControllerPenalCodes/Models/Entities/Status.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ControllerPenalCodes.Models.Entities
+{
+	public class Status
+	{
+		public Guid Id { get; set; }
+
+		public string Name { get; set; }
+	}
+}
