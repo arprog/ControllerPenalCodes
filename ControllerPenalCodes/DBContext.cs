cd9d1f09d558ae6f0e1bade764ad7d5c559f311f
arprog <argit123@hotmail.com>
2022-04-20 16:52:35 -0300

    Creation of context

---
+++ ControllerPenalCodes/DBContext.cs
@@ -0,0 +1,29 @@
+﻿using ControllerPenalCodes.Models.Entities;
+using ControllerPenalCodes.Models.Maps;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControllerPenalCodes
+{
+	public class DBContext : DbContext
+	{
+		public DbSet<CriminalCode> CriminalCodes { get; set; }
+
+		public DbSet<Status> Status { get; set; }
+
+		public DbSet<User> Users { get; set; }
+
+		public DBContext(DbContextOptions<DBContext> options) : base(options)
+		{
+
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<CriminalCode>().Map();
+			modelBuilder.Entity<Status>().Map();
+			modelBuilder.Entity<User>().Map();
+
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
