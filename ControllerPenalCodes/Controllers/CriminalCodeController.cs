4d7d825cec750a3b504677df295f89989bafda3b
arprog <argit123@hotmail.com>
2022-04-23 00:10:04 -0300

    Creation of status controller and creation of criminal code controller

---
+++ ControllerPenalCodes/Controllers/CriminalCodeController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using ControllerPenalCodes.Models.Interfaces.ServiceInterfaces;
+using ControllerPenalCodes.Models.ViewModels.CriminalCodeViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControllerPenalCodes.Controllers
+{
+	[ApiController]
+	[Authorize]
+	[Route("api/v1")]
+	public class CriminalCodeController : ControllerBase
+	{
+		private readonly ICriminalCodeService _criminalCodeService;
+
+		public CriminalCodeController(ICriminalCodeService criminalCodeService)
+		{
+			_criminalCodeService = criminalCodeService;
+		}
+
+		[HttpPost]
+		[Route("criminal-codes")]
+		public async Task<IActionResult> Post([FromBody] CreateCriminalCodeViewModel criminalCodeViewModel)
+		{
+			if (!ModelState.IsValid)
+				return BadRequest();
+
+			try
+			{
+				var userId = User.Claims.FirstOrDefault(i => i.Type.Equals("NameIdentifier")).Value;
+
+				var response = await _criminalCodeService.Create(criminalCodeViewModel, userId);
+
+				return response.Ok ? Created(response.Message, response.Return) : BadRequest(response.Message);
+			}
+			catch (Exception)
+			{
+				return StatusCode(500);
+			}
+		}
+
+		[HttpGet]
+		[Route("criminal-codes")]
+		public async Task<IActionResult> GetAll()
+		{
+			try
+			{
+				var response = await _criminalCodeService.GetAll();
+
+				return response.Ok ? Ok(response.Return) : NoContent();
+			}
+			catch(Exception)
+			{
+				return StatusCode(500);
+			}
+		}
+
+		[HttpGet]
+		[Route("criminal-codes/id/{id}")]
+		public async Task<IActionResult> GetById([FromRoute] Guid id)
+		{
+			try
+			{
+				var response = await _criminalCodeService.GetById(id);
+
+				return response.Ok ? Ok(response.Return) : NoContent();
+			}
+			catch (Exception)
+			{
+				return StatusCode(500);
+			}
+		}
+
+		[HttpPut]
+		[Route("criminal-codes/{id}")]
+		public async Task<IActionResult> Put([FromBody] UpdateCriminalCodeViewModel criminalCodeViewModel)
+		{
+			if (!ModelState.IsValid)
+				return BadRequest();
+
+			try
+			{
+				var userId = User.Claims.FirstOrDefault(i => i.Type.Equals("NameIdentifier")).Value;
+
+				var response = await _criminalCodeService.Update(criminalCodeViewModel, userId);
+
+				if (response.Ok)
+					return Ok();
+
+				return String.IsNullOrEmpty(response.Message) ? NoContent() : BadRequest(response.Message);
+			}
+			catch (Exception)
+			{
+				return StatusCode(500);
+			}
+		}
+
+		[HttpDelete]
+		[Route("criminal-codes/{id}")]
+		public async Task<IActionResult> Delete([FromRoute] Guid id)
+		{
+			try
+			{
+				var response = await _criminalCodeService.Delete(id);
+
+				return response.Ok ? Ok() : NoContent();
+			}
+			catch (Exception)
+			{
+				return StatusCode(500);
+			}
+		}
+	}
+}
---
+++ ControllerPenalCodes/Controllers/StatusController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Threading.Tasks;
+using ControllerPenalCodes.Models.Interfaces.ServiceInterfaces;
+using ControllerPenalCodes.Models.ViewModels.StatusViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControllerPenalCodes.Controllers
+{
+	[ApiController]
+	[Authorize]
+	[Route("api/v1")]
+	public class StatusController : ControllerBase
+	{
+		private readonly IStatusService _statusService;
+
+		public StatusController(IStatusService statusService)
+		{
+			_statusService = statusService;
+		}
+
+		[HttpPost]
+		[Route("status")]
+		public async Task<IActionResult> Post([FromBody] CreateStatusViewModel statusViewModel)
+		{
+			if (!ModelState.IsValid)
+				return BadRequest();
+
+			try
+			{
+				var response = await _statusService.Create(statusViewModel);
+
+				return response.Ok ? Created(response.Message, response.Return) : BadRequest(response.Message);
+			}
+			catch (Exception)
+			{
+				return StatusCode(500);
+			}
+		}
+
+		[HttpGet]
+		[Route("status")]
+		public async Task<IActionResult> GetAll()
+		{
+			try
+			{
+				var response = await _statusService.GetAll();
+
+				return response.Ok ? Ok(response.Return) : NoContent();
+			}
+			catch (Exception)
+			{
+				return StatusCode(500);
+			}
+		}
+
+		[HttpGet]
+		[Route("status/id/{id}")]
+		public async Task<IActionResult> GetById([FromRoute] Guid id)
+		{
+			try
+			{
+				var response = await _statusService.GetById(id);
+
+				return response.Ok ? Ok(response.Return) : NoContent();
+			}
+			catch (Exception)
+			{
+				return StatusCode(500);
+			}
+		}
+
+		[HttpPut]
+		[Route("status/{id}")]
+		public async Task<IActionResult> Put([FromBody] UpdateStatusViewModel statusViewModel)
+		{
+			if (!ModelState.IsValid)
+				return BadRequest();
+
+			try
+			{
+				var response = await _statusService.Update(statusViewModel);
+
+				if (response.Ok)
+					return Ok();
+
+				return String.IsNullOrEmpty(response.Message) ? NoContent() : BadRequest(response.Message);
+			}
+			catch(Exception)
+			{
+				return StatusCode(500);
+			}
+		}
+
+		[HttpDelete]
+		[Route("status/{id}")]
+		public async Task<IActionResult> Delete([FromRoute] Guid id)
+		{
+			try
+			{
+				var response = await _statusService.Delete(id);
+
+				return response.Ok ? Ok() : NoContent();
+			}
+			catch (Exception)
+			{
+				return StatusCode(500);
+			}
+		}
+	}
+}
