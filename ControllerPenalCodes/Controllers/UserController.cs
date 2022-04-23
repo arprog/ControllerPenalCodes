894b93c7085e9700aba8b98cbcf11768d3d76719
arprog <argit123@hotmail.com>
2022-04-23 00:14:09 -0300

    Creation of user controller and user service

---
+++ ControllerPenalCodes/Controllers/UserController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using ControllerPenalCodes.Models.Interfaces.ServiceInterfaces;
+using ControllerPenalCodes.Models.ViewModels.UserViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControllerPenalCodes.Controllers
+{
+	[ApiController]
+	[Authorize]
+	[Route("api/v1")]
+	public class UserController : ControllerBase
+	{
+		private readonly IUserService _userService;
+
+		public UserController(IUserService userService)
+		{
+			_userService = userService;
+		}
+
+		[HttpPost]
+		[AllowAnonymous]
+		[Route("users")]
+		public async Task<IActionResult> Post([FromBody] CreateUserViewModel userViewModel)
+		{
+			if (!ModelState.IsValid)
+				return BadRequest();
+
+			try
+			{
+				var response = await _userService.Create(userViewModel);
+
+				return response.Ok ? Created(response.Message, response.Return) : BadRequest(response.Message);
+			}
+			catch(Exception)
+			{
+				return StatusCode(500);
+			}
+		}
+
+		[HttpGet]
+		[Route("users")]
+		public async Task<IActionResult> GetAll()
+		{
+			try
+			{
+				var response = await _userService.GetAll();
+
+				return response.Ok ? Ok(response.Return) : NoContent();
+			}
+			catch (Exception)
+			{
+				return StatusCode(500);
+			}
+		}
+
+		[HttpGet]
+		[Route("users/id/{id}")]
+		public async Task<IActionResult> GetById([FromRoute] Guid id)
+		{
+			try
+			{
+				var response = await _userService.GetById(id);
+
+				return response.Ok ? Ok(response.Return) : NoContent();
+			}
+			catch (Exception)
+			{
+				return StatusCode(500);
+			}
+		}
+
+		[HttpPut]
+		[Route("users/{id}")]
+		public async Task<IActionResult> Put([FromBody] UpdateUserViewModel userViewModel)
+		{
+			if (!ModelState.IsValid)
+				return BadRequest();
+
+			try
+			{
+				var response = await _userService.Update(userViewModel);
+
+				if (response.Ok)
+					return Ok();
+
+				return String.IsNullOrEmpty(response.Message) ? NoContent() : BadRequest(response.Message);
+			}
+			catch (Exception)
+			{
+				return StatusCode(500);
+			}
+		}
+
+		[HttpDelete]
+		[Route("users/{id}")]
+		public async Task<IActionResult> Delete([FromRoute] Guid id)
+		{
+			try
+			{
+				var response = await _userService.Delete(id);
+
+				return response.Ok ? Ok() : NoContent();
+			}
+			catch (Exception)
+			{
+				return StatusCode(500);
+			}
+		}
+	}
+}
---
+++ ControllerPenalCodes/Services/UserService.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ControllerPenalCodes.Models.Entities;
+using ControllerPenalCodes.Models.Interfaces.RepositoryInterfaces;
+using ControllerPenalCodes.Models.Interfaces.ServiceInterfaces;
+using ControllerPenalCodes.Models.Mappers;
+using ControllerPenalCodes.Models.ViewModels.UserViewModels;
+using ControllerPenalCodes.Utils;
+
+namespace ControllerPenalCodes.Services
+{
+	public class UserService : IUserService
+	{
+		private readonly IUserRepository _userRepository;
+
+		public UserService(IUserRepository userRepository)
+		{
+			_userRepository = userRepository;
+		}
+
+		public async Task<Response<User>> Create(CreateUserViewModel userViewModel)
+		{
+			var user = await _userRepository.Get(userViewModel.Username);
+
+			if (user != null)
+				return Response<User>.ResponseService(false, "There is already user registered with the 'username' informed.");
+
+			user = new User
+			{
+				Id = Guid.NewGuid(),
+				UserName = userViewModel.Username,
+				Password = Security.GenerateHashPassword(userViewModel.Password)
+			};
+
+			await _userRepository.Add(user);
+
+			return Response<User>.ResponseService(true, $"api/v1/users/{user.Id}", user);
+		}
+
+		public async Task<Response<IEnumerable<GetUserViewModel>>> GetAll()
+		{
+			var userList = await _userRepository.GetAll();
+
+			if (userList == null || userList.Count() == 0)
+				return Response<IEnumerable<GetUserViewModel>>.ResponseService(false);
+
+			var userViewModelList = UserMapper.EntityListToViewModelList(userList);
+
+			return Response<IEnumerable<GetUserViewModel>>.ResponseService(true, userViewModelList);
+		}
+
+		public async Task<Response<GetUserViewModel>> GetById(Guid userId)
+		{
+			var user = await _userRepository.Get(userId);
+
+			if (user == null)
+				return Response<GetUserViewModel>.ResponseService(false);
+
+			var userViewModel = UserMapper.EntityToViewModel(user);
+
+			return Response<GetUserViewModel>.ResponseService(true, userViewModel);
+		}
+
+		public async Task<Response<GetUserViewModel>> GetByUsername(string username)
+		{
+			var user = await _userRepository.Get(username);
+
+			if (user == null)
+				return Response<GetUserViewModel>.ResponseService(false);
+
+			var userViewModel = UserMapper.EntityToViewModel(user);
+
+			return Response<GetUserViewModel>.ResponseService(true, userViewModel);
+		}
+
+		public async Task<Response<User>> Update(UpdateUserViewModel newUserViewModel)
+		{
+			var user = await _userRepository.Get(newUserViewModel.Username);
+
+			if (user != null)
+				return Response<User>.ResponseService(false, "There is already user registered with the 'username' informed.");
+
+			user = await _userRepository.Get(newUserViewModel.Id);
+
+			if (user == null)
+				return Response<User>.ResponseService(false);
+
+			var newUser = new User
+			{
+				Id = user.Id,
+				UserName = newUserViewModel.Username,
+				Password = Security.GenerateHashPassword(newUserViewModel.Password)
+			};
+
+			await _userRepository.Update(newUser);
+
+			return Response<User>.ResponseService(true);
+		}
+
+		public async Task<Response<User>> Delete(Guid userId)
+		{
+			var user = await _userRepository.Get(userId);
+
+			if (user == null)
+				return Response<User>.ResponseService(false);
+
+			await _userRepository.Remove(user);
+
+			return Response<User>.ResponseService(true);
+		}
+	}
+}
